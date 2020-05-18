9671bb577f6cb27d6f4394493ff2f4d76fc75658
Hamza Shafiq <hamza.shafiq@sendoso.com>
2020-05-18 22:52:58 +0500

    Updated resume issue for mobile

--- src/components/menu.js
+++ src/components/menu.js
@@ -105,7 +105,7 @@ const Menu = ({ menuOpen, toggleMenu }) => {
                 </NavListItem>
               ))}
           </NavList>
-          <ResumeLink href="/resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
+          <ResumeLink href="/HamzaShafiq.pdf" target="_blank" rel="nofollow noopener noreferrer">
             Resume
           </ResumeLink>
         </NavLinks>
