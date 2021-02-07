837ec58eb9d5f4ae66eee9b2b3d8ac4b06370f73
Hamza Shafiq <hamza.shafiq@sendoso.com>
2021-02-07 23:58:10 +0500

    Updated Text

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Hamza Shafiq | Senior Software Engineer',
+  siteTitle: 'Hamza Shafiq | Tech Lead',
   siteDescription:
     'Full Stack Developer with 4 years of experience, highly effective at developing new programs and fixing problems with existing systems. Proficient in Vue, Ruby On Rails, Python, React, Angular and agile methodology.',
   siteKeywords:
